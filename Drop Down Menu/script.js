87df827c245f80a58d7e7030e035f70b083f68c1
arcygore <rcgore602@gmail.com>
2023-01-22 00:10:54 -0500

    initial commit, menu button working

---
+++ Drop Down Menu/script.js
@@ -0,0 +1,24 @@
+const menuBtn = document.querySelector(".menu-button");
+const menuDropdown = document.querySelector(".menu-dropdown");
+const documentBody = document.querySelector("body");
+
+menuBtn.addEventListener('click', createDropdown)
+menuDropdown.addEventListener('click', dropdownSelect)
+documentBody.addEventListener('click', () => {
+    menuDropdown.classList.remove('active');
+})
+
+function createDropdown(e) {
+    e.stopPropagation();
+    menuDropdown.classList.add('active');
+    menuDropdown.style.left = `${e.clientX}px`
+    menuDropdown.style.top = `${e.clientY}px`
+}
+
+function dropdownSelect(e) {
+    e.stopPropagation();
+    if (e.target.classList.contains('menu-dropdown')) {
+        return
+    } 
+    else menuDropdown.classList.remove('active');
+}
