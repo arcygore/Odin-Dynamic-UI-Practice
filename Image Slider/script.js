17bd93a56bc65c8718fd01784524c3cda2f74bcd
arcygore <rcgore602@gmail.com>
2023-01-23 22:22:00 -0500

    Added code for selector bubbles at bottom of page

--- Image Slider/script.js
+++ Image Slider/script.js
@@ -1,3 +1,15 @@
-const imageSlider = document.querySelector('.image-slider')
-const imageSelector = document.querySelector('image-selector')
+const imageSlider = document.querySelectorAll('.image-slider')
+const imageSelector = document.querySelectorAll('.image-selector')
 
+imageSelector.forEach(element => {
+    element.addEventListener('click', e => {
+        removeClasses(imageSelector[0].children);
+        e.target.classList.add('selected');
+    })
+    });
+
+function removeClasses(arr) {
+    for (item of arr) {
+        item.classList.remove('selected');
+    }
+}
