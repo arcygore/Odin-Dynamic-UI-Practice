0054cf965883ddcc5ddab24f1994a4e37834f5ef
arcygore <rcgore602@gmail.com>
2023-01-25 22:53:42 -0500

    Added link between circle and photo selections

--- Image Slider/script.js
+++ Image Slider/script.js
@@ -4,6 +4,9 @@ const imageSelector = document.querySelectorAll('.selection')
 imageSelector.forEach(element => {
     element.addEventListener('click', e => {
         removeSelectorClasses(imageSelector);
+        removeImageClasses(sliderImages);    
+        const selectedIndex = Array.from(e.target.parentElement.children).indexOf(e.target);
+        sliderImages[selectedIndex].classList.add('active');
         e.target.classList.add('selected');
     })
     });
