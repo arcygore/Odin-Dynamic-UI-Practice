8dd48d8eaf5bff66ac42550e7c37900e889e3b8c
arcygore <rcgore602@gmail.com>
2023-01-26 23:54:58 -0500

    Added auto-slide functionality on a timer - Final

--- Image Slider/script.js
+++ Image Slider/script.js
@@ -1,6 +1,7 @@
 const sliderImages = document.querySelectorAll('.image')
 const imageSelector = document.querySelectorAll('.selection')
 
+
 imageSelector.forEach(element => {
     element.addEventListener('click', e => {
         removeSelectorClasses(imageSelector);
@@ -22,3 +23,17 @@ function removeSelectorClasses(arr) {
         item.classList.remove('selected');
     }
 }
+
+function imageAutoRotate() {
+    let imageIndex = Array.from(imageSelector).indexOf(document.querySelector('.selected'));
+    removeSelectorClasses(imageSelector);
+    removeImageClasses(sliderImages);
+    if (imageIndex == sliderImages.length-1) {
+        imageIndex = 0;
+    } else imageIndex++;
+    sliderImages[imageIndex].classList.add('active');
+    imageSelector[imageIndex].classList.add('selected');
+}
+   
+setInterval(imageAutoRotate, 3000);
+
